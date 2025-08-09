8ef987fc3809b16135b98bea39c168db2f4adf7d
Chirag Raval <raval.chirag26@gmail.com>
2025-08-09 05:24:41 +0000

    Added a Java Maven project with annotation processor

---
+++ daily-stock-screener/src/main/java/com/chirag/stockscreener/DailyStockScreenerDataGenerator.java
@@ -0,0 +1,8 @@
+package com.chirag.stockscreener;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface DailyStockScreenerDataGenerator {
+}
